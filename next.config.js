3d52b5f58d70e8454fcc78151fb3b4101b87a3a7
futurefounder <jsk@posteo.de>
2023-05-10 20:00:18 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: false,
+};
