8a1073a12c4287e08c386503acba363d4a62f898
futurefounder <jsk@posteo.de>
2023-06-12 23:23:36 +0200

    add get directions links

--- src/app/components/BookingCards.tsx
+++ src/app/components/BookingCards.tsx
@@ -82,10 +82,32 @@ export default function BookingCards(params: any) {
             <span className="font-bold ">
               &apos;&apos;Conscious Kiez&apos;&apos; in Friedrichshain
             </span>
-            , a cozy haven where yoga comes alive in an intimate space,
-            exclusively welcoming up to six souls on a transformative journey.
+            , a cozy haven where yoga comes alive in an intimate space, warmly
+            welcoming up to six souls on a transformative journey. <br /> <br />
             {/* </div> */}
           </p>
+        </div>
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            className="w-6 w-6 stroke-fuchsia-600"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
+            />
+          </svg>{" "}
+          <Link
+            target="_blank"
+            className="font-semibold underline decoration-dotted hover:text-fuchsia-800 text-fuchsia-600 "
+            href="https://www.google.com/maps/dir//CONSCIOUS+KIEZ,+Petersburger+Platz,+Berlin/@52.5213364,13.4084965,13z/data=!3m2!4b1!5s0x47a84e32729fb783:0xa07d3be50504bf21!4m9!4m8!1m0!1m5!1m1!1s0x47a84fa601cd17d9:0x2f39514e9a0b452b!2m2!1d13.4496958!2d52.5212913!3e1?entry=ttu"
+          >
+            Get directions{" "}
+          </Link>
         </div>
         <dl className="flex gap-4 mt-6 sm:gap-6">
           <div className="flex flex-col-reverse">
@@ -94,7 +116,7 @@ export default function BookingCards(params: any) {
           </div>
 
           <div className="flex flex-col-reverse">
-            <dt className="text-sm font-medium text-gray-600">Vinyasa Flow</dt>
+            <dt className="text-sm font-medium text-gray-600">Hatha Vinyasa</dt>
             <dd className="text-xs text-gray-500">Style</dd>
           </div>
         </dl>{" "}
@@ -173,11 +195,34 @@ export default function BookingCards(params: any) {
             Seek solace in the tranquil ambiance of the{" "}
             <span className="font-bold">
               {" "}
-              &apos;&apos;Iyengar Yoga Studio&apos;&apos; in Kreuzberg{" "}
+              Iyengar Yogastudio Berlin, Oranienstr. 163, in Kreuzberg{" "}
             </span>
             and immerse yourself in a space that warmly envelops up to 10 souls,
-            nurturing deep connections.
+            nurturing deep connections. <br />
+            <br />
           </p>
+        </div>{" "}
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            className="w-6 w-6 stroke-fuchsia-600"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
+            />
+          </svg>{" "}
+          <Link
+            target="_blank"
+            className="font-semibold underline decoration-dotted hover:text-fuchsia-800 text-fuchsia-600 "
+            href="https://www.google.com/maps/dir//Iyengar+Yogastudio+Berlin,+Oranienstra%C3%9Fe+163,+10969+Berlin/@52.5025244,13.3863662,14z/data=!3m1!5s0x47a84e32729fb783:0xa07d3be50504bf21!4m18!1m8!3m7!1s0x47a84e326d4fb363:0xdaf3f08c1aea866e!2sIyengar+Yogastudio+Berlin!8m2!3d52.5025238!4d13.4141036!15sChNpeWVuZ2FyIHlvZ2EgYmVybGlukgELeW9nYV9zdHVkaW_gAQA!16s%2Fg%2F1w2yyh1j!4m8!1m0!1m5!1m1!1s0x47a84e326d4fb363:0xdaf3f08c1aea866e!2m2!1d13.4141036!2d52.5025238!3e1?entry=ttu"
+          >
+            Get directions{" "}
+          </Link>
         </div>
         <dl className="flex gap-4 mt-6 sm:gap-6">
           <div className="flex flex-col-reverse">
@@ -186,7 +231,7 @@ export default function BookingCards(params: any) {
           </div>
 
           <div className="flex flex-col-reverse">
-            <dt className="text-sm font-medium text-gray-600">Vinyasa Flow</dt>
+            <dt className="text-sm font-medium text-gray-600">Hatha Vinyasa</dt>
             <dd className="text-xs text-gray-500">Style</dd>
           </div>
         </dl>{" "}
