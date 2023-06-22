8babd98fc563953b6db4dc1eab40fa954e821bed
futurefounder <jsk@posteo.de>
2023-06-22 12:44:30 +0200

    add test-blog

---
+++ src/app/components/Posts.tsx
@@ -0,0 +1,16 @@
+import { getSortedPostsData } from "../lib/posts";
+import ListItem from "./ListItem";
+
+export default function Posts() {
+  const posts = getSortedPostsData();
+  return (
+    <section className="max-w-2xl mx-auto mt-6">
+      <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2>
+      <ul className="w-full">
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
+}
---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,71 @@
+import getFormattedDate from "../../lib/getFormattedDate";
+import { getSortedPostsData, getPostData } from "../..//lib/posts";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+
+export function generateStaticParams() {
+  const posts = getSortedPostsData();
+
+  return posts.map((post) => ({
+    postId: post.id,
+  }));
+}
+
+export function generateMetadata({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData();
+  const { postId } = params;
+
+  const post = posts.find((post) => post.id === postId);
+
+  if (!post) {
+    return {
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.title,
+  };
+}
+
+export default async function Post({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData();
+  const { postId } = params;
+
+  if (!posts.find((post) => post.id === postId)) notFound();
+
+  const { title, date, contentHtml } = await getPostData(postId);
+
+  const pubDate = getFormattedDate(date);
+
+  return (
+    <main className="px-6 mx-auto prose prose-xl prose-slate dark:prose-invert">
+      <h1 className="mt-4 mb-0 text-3xl">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          {" "}
+          <br />
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.8}
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+              />
+            </svg>
+            <Link href="/blog">&nbsp; Back to blog</Link>
+          </div>
+        </p>
+      </article>
+    </main>
+  );
+}
