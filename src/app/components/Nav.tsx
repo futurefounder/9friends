8551d48ff34fd3cf0a756a1da36c9422cdaca7a3
futurefounder <jsk@posteo.de>
2023-07-03 07:52:22 +0200

    edit sequence to session, add note signup NL

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -127,14 +127,14 @@ export default function Navigation() {
             <li>
               {" "}
               <Link
-                href="/sequences"
+                href="/sessions"
                 className={
-                  pathname == "/sequences"
+                  pathname == "/sessions"
                     ? "underline text-purple-600 block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     : " block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 }
               >
-                Sequences{" "}
+                Sessions{" "}
               </Link>{" "}
             </li>
             {/* <li>
